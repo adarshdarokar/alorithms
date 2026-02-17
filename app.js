7a7988e6e6c812c3041d539fe017437021fc0319
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-02-17 23:08:16 +0530

    finding k-elem commit

--- app.js
+++ app.js
@@ -769,4 +769,16 @@
 //.........  arrray left rotation by k elements .......... //
 
 
-let arr = [1, 2, 3, 4, 5]
+let arr = [1, 2, 3, 4, 5];
+let k = 2;
+for (let j = 1; j <= k; j++) {
+
+
+    let copy = arr[0]
+    for (let i = 0; i < arr.length - 1; i++) {
+        arr[i] = arr[i + 1]
+    }
+    arr[arr.length - 1] = copy
+}
+    console.log(arr);
+
