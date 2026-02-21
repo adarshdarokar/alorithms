de26e447ad953a1f745eff9d78cfe6f992df3299
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-02-21 15:24:25 +0530

    Selection Sort Algorithm - commit

--- app.js
+++ app.js
@@ -883,6 +883,7 @@
 
 //...... Moving On To Sorting ......//
 
+    //..... this is bubble .....//
 
 // let arr = [1, 4, 3, 2, 9, 18];
 // let n = arr.length
@@ -899,5 +900,24 @@
 // console.log(arr);
 
 
+//..........  Selection Sort Algorithm   ...........//
+
+// let arr = [10, 3, 9, 4, 7];
+// for (let i = 0; i < arr.length - 1; i++) {
+//     let small = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[small] > arr[j])
+//             small = j;
+//     }
+//     if (i != small) {
+//         let temp = arr[i]
+//         arr[i] = arr[small];
+//         arr[small] = temp;
+//     }
+// }
+
+// console.log(arr);
 
 
+
+-
